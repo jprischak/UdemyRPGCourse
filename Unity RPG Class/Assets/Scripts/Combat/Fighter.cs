0eb73bd80b7903da31c9167d2db727b61747f8c2
jprischak <jprischak@gmail.com>
2021-07-14 21:31:08 -0400

    Implementing Action Priorities

---
+++ Unity RPG Class/Assets/Scripts/Combat/Fighter.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace RPG.Combat
+{
+
+
+    public class Fighter : MonoBehaviour
+    {
+        /**
+    *  VARIABLES
+    * */
+        // Constant
+
+
+
+        // Public
+
+
+
+        // Serialized
+
+
+
+        // Private
+
+
+
+
+
+
+        /**
+        *  CLASS FUNCTIONS
+        * */
+        private void Start()
+        {
+
+        }
+
+
+        private void Update()
+        {
+
+        }
+
+
+        public void Attack(CombatTarget target)
+        {
+            print("Attack!");
+        }
+
+    }
+}
--- Unity RPG Class/Assets/Scripts/Movement/Mover.cs
+++ Unity RPG Class/Assets/Scripts/Movement/Mover.cs
@@ -6,69 +6,68 @@ using UnityEngine.AI;
 
 
 
-
-
-
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
-    /**
-	*  VARIABLES
-	* */
-    // Constant
 
 
-
-    // Public
-    Ray lastRay;
-
-
-	// Serialized
-	[SerializeField] Transform target;
-
-
-	// Private
-
-
-	/**
-	*	CLASS FUNCTIONS
-	* */
-	private void Start()
-	{
-	}
-
-
-
-
-	private void Update()
-	{
-        
-        UpdateAnimator();
-
-    }
-
-
-
-   
-
-
-    public void MoveTo(Vector3 destination)
+    public class Mover : MonoBehaviour
     {
-        GetComponent<NavMeshAgent>().destination = destination;
-    }
+        /**
+        *  VARIABLES
+        * */
+        // Constant
 
 
-    private void UpdateAnimator()
-    {
-        // This gets our velocity from the navMeshAgent
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
 
-        // This will convert the velocity from world to local
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        // Public
+        Ray lastRay;
 
-        // Save our speed from our local velocity
-        float speed = localVelocity.z;
 
-        // Store our speed variable in the animator variable
-        GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        // Serialized
+        [SerializeField] Transform target;
+
+
+        // Private
+
+
+        /**
+        *	CLASS FUNCTIONS
+        * */
+        private void Start()
+        {
+        }
+
+
+        private void Update()
+        {
+
+            UpdateAnimator();
+
+        }
+
+
+
+
+
+        public void MoveTo(Vector3 destination)
+        {
+            GetComponent<NavMeshAgent>().destination = destination;
+        }
+
+
+        private void UpdateAnimator()
+        {
+            // This gets our velocity from the navMeshAgent
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+
+            // This will convert the velocity from world to local
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            // Save our speed from our local velocity
+            float speed = localVelocity.z;
+
+            // Store our speed variable in the animator variable
+            GetComponent<Animator>().SetFloat("ForwardSpeed", speed);
+        }
     }
 }
