81df99995440207b39ab51ef6f3a3619345e4ecb
jprischak <jprischak@gmail.com>
2021-08-19 19:50:41 -0400

    Triggering Saving and Loading

---
+++ Unity RPG Class/Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        /**
+        *  VARIABLES
+        * */
+        // Constant
+        const string DEFAULTSAVEFILE = "save";
+
+        // Public
+
+
+        // Serialized
+
+
+        // Private
+
+
+
+
+        /**
+        *	CORE FUNCTIONS
+        * */
+        public void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if(Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+
+
+        /**
+         *  CLASS FUNCTIONS
+         * */
+        public void Load()
+        {
+            GetComponent<SavingSystem>().Load(DEFAULTSAVEFILE);
+        }
+
+        public void Save()
+        {
+            GetComponent<SavingSystem>().Save(DEFAULTSAVEFILE);
+        }
+    }
+}
